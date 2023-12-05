47d05accf26cb615824c1f743f4a8dd2d0cabc2c
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2023-12-05 11:33:49 +0100

    untested poc implementation of the gateway

---
+++ module-gateway/src/main.rs
@@ -0,0 +1,82 @@
+#![no_main]
+#![no_std]
+#![macro_use]
+#![feature(type_alias_impl_trait)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::usart;
+use lora_phy::mod_params::RadioError;
+use module_runtime::*;
+
+type UartBuffer = [u8; 128];
+
+#[derive(Debug, defmt::Format, PartialEq)]
+enum Error {
+    MessageTooShort,
+    UnknownType,
+    Usart(usart::Error),
+    LoRa(RadioError),
+}
+
+async fn lora_message(
+    module: &mut ModuleInterface,
+    tx_buffer: &[u8],
+    rx_buffer: &mut [u8],
+) -> Result<u8, Error> {
+    module.lora_transmit(tx_buffer).await.map_err(Error::LoRa)?;
+    module.lora_receive(rx_buffer).await.map_err(Error::LoRa)
+}
+
+async fn process_host_message(
+    module: &mut ModuleInterface,
+    uart_buffer: &[u8],
+) -> Result<(), Error> {
+    if uart_buffer.len() == 0 {
+        return Err(Error::MessageTooShort);
+    }
+    match uart_buffer[0] {
+        // ping
+        0 => {
+            module.uart.write(uart_buffer).await.map_err(Error::Usart)?;
+        }
+        // transmit lora
+        1 => {
+            if uart_buffer.len() > 1 {
+                let mut rx = [0u8; 128];
+                lora_message(module, &uart_buffer[1..], &mut rx[1..]).await?;
+                rx[0] = 1;
+                module.uart.write(uart_buffer).await.map_err(Error::Usart)?;
+            }
+        }
+        // unhandled
+        _ => {
+            return Err(Error::UnknownType);
+        }
+    }
+    Ok(())
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut module = init().await;
+
+    let mut uart_buffer: UartBuffer = [0u8; 128];
+    loop {
+        let result = module.uart.read_until_idle(&mut uart_buffer).await;
+        match result {
+            Ok(size) => {
+                info!("size {}", size);
+                match process_host_message(&mut module, uart_buffer.as_slice()).await {
+                    Ok(()) => {}
+                    Err(e) => {
+                        error!("{}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                error!("uart: {}", e);
+            }
+        }
+    }
+}
--- module-runtime/src/lib.rs
+++ module-runtime/src/lib.rs
@@ -1,11 +1,12 @@
 #![no_std]
 #![macro_use]
 #![feature(type_alias_impl_trait)]
-#![allow(stable_features, unknown_lints, async_fn_in_trait)]
+#![allow(stable_features, unknown_lints, async_fn_in_trait, dead_code)]
 
 pub use cortex_m;
 pub use cortex_m_rt;
 pub use defmt;
+use defmt::info;
 pub use defmt_rtt;
 pub use embassy_embedded_hal;
 pub use embassy_executor;
@@ -15,6 +16,7 @@ pub use embassy_time;
 pub use embedded_storage;
 pub use futures;
 pub use heapless;
+pub use lora_phy;
 pub use panic_probe;
 
 use embassy_lora::iv::Stm32wlInterfaceVariant;
@@ -28,6 +30,8 @@ use embassy_time::Delay;
 use lora_phy::mod_params::*;
 use lora_phy::sx1261_2::SX1261_2;
 use lora_phy::LoRa;
+
+const LORA_FREQUENCY_IN_HZ: u32 = 869_525_000; // warning: set this appropriately for the region
 
 bind_interrupts!(struct Irqs{
     LPUART1 => usart::InterruptHandler<peripherals::LPUART1>;
@@ -44,6 +48,45 @@ pub struct ModuleInterface {
         Delay,
     >,
     pub led: Output<'static, AnyPin>,
+
+    lora_modulation: ModulationParams,
+    lora_tx_params: PacketParams,
+    lora_rx_params: PacketParams,
+}
+
+impl ModuleInterface {
+    pub async fn lora_transmit(&mut self, tx_buffer: &[u8]) -> Result<(), RadioError> {
+        self.lora
+            .prepare_for_tx(&self.lora_modulation, 14, false)
+            .await?;
+        self.lora
+            .tx(
+                &self.lora_modulation,
+                &mut self.lora_tx_params,
+                tx_buffer,
+                500,
+            )
+            .await
+    }
+
+    pub async fn lora_receive(&mut self, rx_buffer: &mut [u8]) -> Result<u8, RadioError> {
+        self.lora
+            .prepare_for_rx(
+                &self.lora_modulation,
+                &self.lora_rx_params,
+                None,
+                None,
+                false,
+            )
+            .await?;
+        match self.lora.rx(&self.lora_rx_params, rx_buffer).await {
+            Ok((received_len, status)) => {
+                info!("RX rssi {} len {}", status.rssi, received_len);
+                Ok(received_len)
+            }
+            Err(err) => Err(err),
+        }
+    }
 }
 
 pub async fn init() -> ModuleInterface {
@@ -69,13 +112,30 @@ pub async fn init() -> ModuleInterface {
     let ctrl2 = Output::new(p.PC5.degrade(), Level::High, Speed::High);
     let iv = Stm32wlInterfaceVariant::new(Irqs, None, Some(ctrl2)).unwrap();
 
-    let lora = LoRa::new(
+    let mut lora = LoRa::new(
         SX1261_2::new(BoardType::Stm32wlSx1262, spi, iv),
         false,
         Delay,
     )
     .await
     .unwrap();
+
+    let lora_modulation = lora
+        .create_modulation_params(
+            SpreadingFactor::_10,
+            Bandwidth::_250KHz,
+            CodingRate::_4_8,
+            LORA_FREQUENCY_IN_HZ,
+        )
+        .unwrap();
+
+    let lora_tx_params = lora
+        .create_tx_packet_params(4, false, true, false, &lora_modulation)
+        .unwrap();
+
+    let lora_rx_params = lora
+        .create_rx_packet_params(4, false, 128u8, true, false, &lora_modulation)
+        .unwrap();
 
     let mut lpuart1_config = usart::Config::default();
     lpuart1_config.baudrate = 115200;
@@ -96,5 +156,8 @@ pub async fn init() -> ModuleInterface {
         uart: lpuart1,
         lora,
         led,
+        lora_modulation,
+        lora_tx_params,
+        lora_rx_params,
     }
 }
