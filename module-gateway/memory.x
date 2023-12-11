8d44ee707e1ffb6216c85248b8c64a95f504bc05
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2023-12-11 21:59:34 +0100

    uart ping still works

--- module-gateway/memory.x
+++ module-gateway/memory.x
@@ -1,6 +1,6 @@
-/* Linker script for the STM32F103C8T6 */
+/* Linker script for the STM32WLE5CC */
 MEMORY
 {
   FLASH : ORIGIN = 0x08000000, LENGTH = 256K
-  RAM : ORIGIN = 0x20000000, LENGTH = 20K
+  RAM : ORIGIN = 0x20000000, LENGTH = 64K
 }
--- updater/updater.py
+++ updater/updater.py
@@ -1,10 +1,28 @@
 
 from updater.protocol import Usb
+from pathlib import Path
+
 
 
 if __name__ == '__main__':
-    gateway = Usb('/dev/ttyACM0', baudrate=115200)
-    print(gateway.reader.serial.baudrate)
+    baudrate = 115200
+    device_path = None
+    for path in Path('/dev/').glob('ttyACM*'):
+        try:
+            gateway = Usb(str(path), baudrate=baudrate)
+            data = bytearray([0, 1, 2, 255, 255, 5, 1])
+            gateway.transmit(data)
+            assert gateway.get_received(timeout=0.1) == data
+            gateway.close()
+            device_path = str(path)
+            break
+        except:
+            pass
+    else:
+        raise RuntimeError('No gateway found')
+
+
+    gateway = Usb(device_path, baudrate=baudrate)
     assert gateway.is_open()
 
     gateway.transmit(bytearray([0, 1, 2, 255, 255, 5, 1, 2, 3, 254, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]))
