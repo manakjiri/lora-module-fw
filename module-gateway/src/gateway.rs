8b501a965fd8449f8e714218541996b6d5ebd36b
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2023-12-30 10:53:49 +0100

    split gateway lora handling into a separate task

---
+++ module-gateway/src/gateway.rs
@@ -0,0 +1,124 @@
+use defmt::*;
+use embassy_stm32::usart;
+use gateway_host_schema::{self, HostPacket};
+use module_runtime::{gateway_host_schema::GatewayPacket, heapless::Vec, *};
+
+#[derive(Debug, defmt::Format, PartialEq)]
+pub enum Error {
+    Usart(usart::Error),
+    SerDe(postcard::Error),
+    Ota(OtaError),
+}
+
+pub struct Gateway {
+    ota: Option<OtaProducer>,
+}
+
+impl Gateway {
+    pub fn new() -> Gateway {
+        Gateway { ota: None }
+    }
+
+    async fn init_download(
+        &mut self,
+        lora: &mut ModuleLoRa,
+        init: gateway_host_schema::OtaInitRequest,
+    ) -> Result<GatewayPacket, Error> {
+        let mut ota = OtaProducer::new(OtaInitPacket {
+            binary_size: init.binary_size,
+            block_size: init.block_size,
+            binary_sha256: init.binary_sha256,
+        });
+        let ret = ota.init_download(lora).await.map_err(Error::Ota)?;
+        self.ota = Some(ota);
+        Ok(ret)
+    }
+
+    async fn continue_download(
+        &mut self,
+        lora: &mut ModuleLoRa,
+        data: gateway_host_schema::OtaData,
+    ) -> Result<(), Error> {
+        match self.ota.as_mut() {
+            Some(ota) => {
+                ota.continue_download(
+                    lora,
+                    OtaDataPacket {
+                        index: data.index,
+                        data: data.data,
+                    },
+                )
+                .await
+                .map_err(Error::Ota)?;
+            }
+            None => {
+                return Err(Error::Ota(OtaError::NotStarted));
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn process_host_message(
+        &mut self,
+        lora: &mut ModuleLoRa,
+        packet: HostPacket,
+    ) -> Result<Option<GatewayPacket>, Error> {
+        let ret = match packet {
+            HostPacket::PingRequest => Some(GatewayPacket::PingResponse),
+            HostPacket::OtaInit(init) => {
+                info!("init download");
+                match self.ota.as_mut() {
+                    Some(ota) => {
+                        if ota.is_done() {
+                            Some(self.init_download(lora, init).await?)
+                        } else {
+                            return Err(Error::Ota(OtaError::AlreadyStarted));
+                        }
+                    }
+                    None => Some(self.init_download(lora, init).await?),
+                }
+            }
+            HostPacket::OtaData(data) => {
+                info!("continue download");
+                self.continue_download(lora, data).await?;
+                None
+            }
+            HostPacket::OtaAbort => {
+                if let Some(ota) = self.ota.as_mut().take() {
+                    Some(ota.abort_download(lora).await.map_err(Error::Ota)?)
+                } else {
+                    Some(GatewayPacket::OtaAbortAck) //FIXME this should return invalid command or something
+                }
+            }
+            HostPacket::OtaGetStatus => {
+                let packet = GatewayPacket::OtaStatus({
+                    if let Some(ota) = self.ota.as_ref() {
+                        ota.get_status()
+                    } else {
+                        gateway_host_schema::OtaStatus {
+                            in_progress: false,
+                            not_acked: Vec::new(),
+                        }
+                    }
+                });
+                Some(packet)
+            }
+        };
+        Ok(ret)
+    }
+
+    pub async fn process_peer_message(
+        &mut self,
+        lora: &mut ModuleLoRa,
+        lora_buffer: &[u8],
+    ) -> Result<Option<GatewayPacket>, Error> {
+        match self.ota.as_mut() {
+            Some(ota) => Ok(Some(
+                ota.process_response(lora, lora_buffer)
+                    .await
+                    .map_err(Error::Ota)?,
+            )),
+            None => Ok(None),
+        }
+    }
+}
--- module-gateway/src/main.rs
+++ module-gateway/src/main.rs
@@ -3,205 +3,103 @@
 #![macro_use]
 #![feature(type_alias_impl_trait)]
 
+mod gateway;
+
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_futures::select::*;
-use embassy_stm32::usart;
-use gateway_host_schema::{self, HostPacket};
-//use lora_phy::mod_params::RadioError;
-use module_runtime::{gateway_host_schema::GatewayPacket, heapless::Vec, *};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+use gateway::*;
+use gateway_host_schema::{GatewayPacket, HostPacket};
+use module_runtime::*;
 
-#[derive(Debug, defmt::Format, PartialEq)]
-enum Error {
-    Usart(usart::Error),
-    //LoRa(RadioError),
-    SerDe(postcard::Error),
-    Ota(OtaError),
-}
+static HOST2GATEWAY: Channel<ThreadModeRawMutex, HostPacket, 2> = Channel::new();
+static GATEWAY2HOST: Channel<ThreadModeRawMutex, GatewayPacket, 2> = Channel::new();
 
-struct Gateway {
-    ota: Option<OtaProducer>,
-}
-
-impl Gateway {
-    fn new() -> Gateway {
-        Gateway { ota: None }
-    }
-
-    async fn host_write(
-        &mut self,
-        host: &mut ModuleHost,
-        packet: GatewayPacket,
-    ) -> Result<(), Error> {
-        let mut tx_buffer = [0u8; 256];
-        host.write(&postcard::to_slice(&packet, &mut tx_buffer).map_err(Error::SerDe)?)
-            .await
-            .map_err(Error::Usart)
-    }
-
-    async fn init_download(
-        &mut self,
-        host: &mut ModuleHost,
-        lora: &mut ModuleLoRa,
-        init: gateway_host_schema::OtaInitRequest,
-    ) -> Result<(), Error> {
-        let mut ota = OtaProducer::new(OtaInitPacket {
-            binary_size: init.binary_size,
-            block_size: init.block_size,
-            binary_sha256: init.binary_sha256,
-        });
-        self.host_write(host, ota.init_download(lora).await.map_err(Error::Ota)?)
-            .await?;
-        self.ota = Some(ota);
-        Ok(())
-    }
-
-    async fn continue_download(
-        &mut self,
-        _host: &mut ModuleHost,
-        lora: &mut ModuleLoRa,
-        data: gateway_host_schema::OtaData,
-    ) -> Result<(), Error> {
-        match self.ota.as_mut() {
-            Some(ota) => {
-                ota.continue_download(
-                    lora,
-                    OtaDataPacket {
-                        index: data.index,
-                        data: data.data,
-                    },
-                )
-                .await
-                .map_err(Error::Ota)?;
-            }
-            None => {
-                return Err(Error::Ota(OtaError::NotStarted));
-            }
-        }
-        Ok(())
-    }
-
-    async fn process_host_message(
-        &mut self,
-        host: &mut ModuleHost,
-        lora: &mut ModuleLoRa,
-        uart_buffer: &[u8],
-    ) -> Result<(), Error> {
-        match postcard::from_bytes::<HostPacket>(uart_buffer).map_err(Error::SerDe)? {
-            HostPacket::PingRequest => {
-                self.host_write(host, GatewayPacket::PingResponse).await?;
-            }
-            HostPacket::OtaInit(init) => {
-                info!("init download");
-                match self.ota.as_mut() {
-                    Some(ota) => {
-                        if ota.is_done() {
-                            self.init_download(host, lora, init).await?;
-                        } else {
-                            return Err(Error::Ota(OtaError::AlreadyStarted));
-                        }
-                    }
-                    None => {
-                        self.init_download(host, lora, init).await?;
-                    }
-                }
-            }
-            HostPacket::OtaData(data) => {
-                info!("continue download");
-                self.continue_download(host, lora, data).await?;
-            }
-            HostPacket::OtaAbort => {
-                if let Some(ota) = self.ota.as_mut().take() {
-                    ota.abort_download(lora).await.map_err(Error::Ota)?;
-                }
-            }
-            HostPacket::OtaGetStatus => {
-                let packet = GatewayPacket::OtaStatus({
-                    if let Some(ota) = self.ota.as_ref() {
-                        ota.get_status()
-                    } else {
-                        gateway_host_schema::OtaStatus {
-                            in_progress: false,
-                            not_acked: Vec::new(),
-                        }
-                    }
-                });
-                self.host_write(host, packet).await?;
-            }
-        }
-        Ok(())
-    }
-
-    async fn process_peer_message(
-        &mut self,
-        host: &mut ModuleHost,
-        lora: &mut ModuleLoRa,
-        lora_buffer: &[u8],
-    ) -> Result<(), Error> {
-        match self.ota.as_mut() {
-            Some(ota) => {
-                let packet = ota
-                    .process_response(lora, lora_buffer)
-                    .await
-                    .map_err(Error::Ota)?;
-
-                self.host_write(host, packet).await?;
-            }
-            None => {}
-        }
-        Ok(())
-    }
-}
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let module = init(ModuleConfig::new(ModuleVersion::NucleoWL55JC)).await;
-    let mut gateway = Gateway::new();
-    info!("hello from gateway");
-
-    _spawner.spawn(status_led_task(module.led)).unwrap();
-    let mut host = module.host;
-    let mut lora = module.lora;
-
-    let mut uart_buffer = [0u8; 128];
+#[embassy_executor::task]
+pub async fn gateway_task(mut lora: ModuleLoRa) {
+    let mut gw = Gateway::new();
     let mut lora_buffer = [0u8; 128];
     loop {
-        let interfaces = select(host.read(&mut uart_buffer), lora.receive(&mut lora_buffer));
-        match interfaces.await {
-            Either::First(uart_result) => match uart_result {
-                Ok(size) => {
-                    //info!("size {}", size);
-                    match gateway
-                        .process_host_message(&mut host, &mut lora, &uart_buffer[..size])
-                        .await
-                    {
-                        Ok(()) => {}
-                        Err(e) => {
-                            error!("uart: {}", e);
-                        }
+        match select(HOST2GATEWAY.receive(), lora.receive(&mut lora_buffer)).await {
+            Either::First(p) => match gw.process_host_message(&mut lora, p).await {
+                Ok(resp) => {
+                    if let Some(r) = resp {
+                        GATEWAY2HOST.send(r).await;
                     }
                 }
                 Err(e) => {
-                    error!("uart: {}", e);
+                    error!("failed to process host message: {}", e);
                 }
             },
             Either::Second(lora_result) => match lora_result {
                 Ok(len) => {
-                    match gateway
-                        .process_peer_message(&mut host, &mut lora, &lora_buffer[..len])
+                    match gw
+                        .process_peer_message(&mut lora, &lora_buffer[..len])
                         .await
                     {
-                        Ok(()) => {}
+                        Ok(resp) => {
+                            if let Some(r) = resp {
+                                GATEWAY2HOST.send(r).await;
+                            }
+                        }
                         Err(e) => {
-                            error!("lora: {}", e);
+                            error!("failed to process peer message: {}", e);
                         }
                     }
+                    status_led(LedCommand::FlashShort).await;
                 }
                 Err(e) => {
-                    error!("lora: {}", e);
+                    error!("failed lora receive: {}", e);
                 }
             },
         }
-        status_led(LedCommand::FlashShort).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let module = init(ModuleConfig::new(ModuleVersion::NucleoWL55JC)).await;
+
+    info!("hello from gateway");
+
+    spawner.spawn(status_led_task(module.led)).unwrap();
+    spawner.spawn(gateway_task(module.lora)).unwrap();
+    let mut host = module.host;
+
+    let mut uart_buffer = [0u8; 128];
+    loop {
+        match select(host.read(&mut uart_buffer), GATEWAY2HOST.receive()).await {
+            Either::First(uart_result) => {
+                match uart_result {
+                    Ok(size) => match postcard::from_bytes::<HostPacket>(&uart_buffer[..size]) {
+                        Ok(p) => {
+                            HOST2GATEWAY.send(p).await;
+                        }
+                        Err(e) => {
+                            error!("failed to parse packet from host: {}", e);
+                        }
+                    },
+                    Err(e) => {
+                        error!("uart: {}", e);
+                    }
+                }
+                status_led(LedCommand::FlashShort).await;
+            }
+            Either::Second(p) => {
+                let mut tx_buffer = [0u8; 256];
+                match postcard::to_slice(&p, &mut tx_buffer) {
+                    Ok(b) => {
+                        if let Err(_e) = host.write(&b).await {
+                            error!("failed to transmit packet to host");
+                        }
+                    }
+                    Err(e) => {
+                        error!("failed to serialize packet: {}", e);
+                    }
+                }
+            }
+        }
     }
 }
